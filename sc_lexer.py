518fd3fcc009d17ccd1880854365cbdd50064336
somator <torysoma@gmail.com>
2020-10-05 03:02:49 -0500

    Token class, lexer

---
+++ sc_lexer.py
@@ -0,0 +1,36 @@
+import sys
+import re
+
+class Token:
+    def __init__(self, data, tokenType):
+        self.data = data
+        self.tokenType = tokenType
+
+    def __str__(self):
+        return f'<{self.tokenType} Token: {self.data}>'
+    
+    def __repr__(self):
+        return str(self)
+
+
+def lex(characters, token_exprs):
+    pos = 0
+    tokens = []
+    while pos < len(characters):
+        match = None
+        for token_expr in token_exprs:
+            pattern, tokenType = token_expr
+            regex = re.compile(pattern)
+            match = regex.match(characters, pos)
+            if match:
+                text = match.group(0)
+                if tokenType:
+                    token = Token(text, tokenType)
+                    tokens.append(token)
+                break
+        if not match:
+            sys.stderr.write('Illegal character: %s\n' % characters[pos])
+            sys.exit(1)
+        else:
+            pos = match.end(0)
+    return tokens
