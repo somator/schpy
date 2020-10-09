73c02702eafd798d103d2d2c5cf8d4f02357232f
somator <torysoma@gmail.com>
2020-10-09 04:16:40 -0500

    mathematical conditionals

--- sc_eval.py
+++ sc_eval.py
@@ -27,6 +27,14 @@ def eval_exp(exp, env):
         return eval_exp(exp.children[0], env) - sum([eval_exp(sub_exp, env) for sub_exp in exp.children[1:]])
     elif exp.data == '/':
         return eval_exp(exp.children[0], env) / math.prod([eval_exp(sub_exp, env) for sub_exp in exp.children[1:]])
+    elif exp.data == '<=':
+        return eval_exp(exp.children[0], env) <= eval_exp(exp.children[1], env)
+    elif exp.data == '>=':
+        return eval_exp(exp.children[0], env) >= eval_exp(exp.children[1], env)
+    elif exp.data == '<':
+        return eval_exp(exp.children[0], env) < eval_exp(exp.children[1], env)
+    elif exp.data == '>':
+        return eval_exp(exp.children[0], env) > eval_exp(exp.children[1], env)
     elif exp.data == 'define':
         new_exp_var = exp.children[0]
         new_exp_val = exp.children[1]
